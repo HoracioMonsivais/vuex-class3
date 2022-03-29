f14a4ba9c69725a77b5ecfa958ee2b96b81252ec
Horacio Monsivais <ms.horacioo@hotmail.com>
2022-03-28 19:51:05 -0600

    Home

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0
   },
   getters: {
   },
   mutations: {
+    subirContador(state) {
+      state.counter++;
+    },
+    bajarContador(state) {
+      state.counter--;
+    }
   },
   actions: {
+    async subirContador() {
+      const res = await fetch('https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new');
+      const results = await res.json();
+      console.log(results);
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="counter">
+      {{ $store.state.counter }}
+    </div>
+    <div class="buttons">
+      <button @click="$store.commit('bajarContador')">-</button>
+      <button @click="$store.commit('subirContador')">+</button>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style>
+
+div {
+  margin-bottom: 10px;
+}
+
+.counter{
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+
+}
+</style>
